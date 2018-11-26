9295e3f0cf2f297d9f1ac69fe3173588c24899e1
Calin <botofancalin@gmail.com>
2018-11-26 23:42:35 +0200

    updated button defines

--- components/drv/input.h
+++ components/drv/input.h
@@ -6,14 +6,9 @@
 
 lv_indev_t *kp_indev; 
 
-// enum
-// {
 #define	BTN_LEFT    GPIO_NUM_32
 #define	BTN_SELECT  GPIO_NUM_33
 #define	BTN_RIGHT   GPIO_NUM_27
-
-// 	BTN_MAX
-// };
 
 void keypad_init();
 bool keypad_read(lv_indev_data_t *data);
